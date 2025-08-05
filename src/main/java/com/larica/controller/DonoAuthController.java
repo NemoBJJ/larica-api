f5f2b89d67bd8e840d9cdb178298571c4fc611ed
NemoBJJ <engnfaraujo@gmail.com>
2025-08-04 21:05:16 -0300

    LaricaIndo2

--- src/main/java/com/larica/controller/DonoAuthController.java
+++ src/main/java/com/larica/controller/DonoAuthController.java
@@ -4,6 +4,8 @@ import com.larica.entity.DonoRestaurante;
 import com.larica.repository.DonoRestauranteRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.Optional;
 
@@ -35,5 +37,22 @@ public class DonoAuthController {
             "email", dono.getEmail(),
             "telefone", dono.getTelefone()
         ));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerDono(@RequestBody DonoRestaurante request) {
+        if (donoRepository.existsByEmail(request.getEmail())) {
+            return ResponseEntity.badRequest().body("E-mail já cadastrado");
+        }
+
+        DonoRestaurante novoDono = new DonoRestaurante();
+        novoDono.setNome(request.getNome());
+        novoDono.setEmail(request.getEmail());
+        novoDono.setSenha(request.getSenha());
+        novoDono.setTelefone(request.getTelefone());
+        novoDono.setDataCadastro(LocalDate.now());
+
+        donoRepository.save(novoDono);
+        return ResponseEntity.ok("Dono cadastrado com sucesso");
     }
 }
